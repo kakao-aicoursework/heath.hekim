b2ed4025e563b916e70b16781558054f6e2fa43b
heath <heath.hekim@kakaocorp.com>
2023-09-22 17:45:13 +0900

    memory buffer 추가, 검색할 파일 대상 추가

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -13,28 +13,17 @@ load_dotenv()
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-from langchain.agents.tools import Tool
-from langchain.agents import initialize_agent, AgentType
-from langchain.llms import OpenAI
 from langchain.document_loaders import TextLoader
-
-from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import (
-    SystemMessage
-)
-
-from langchain.document_loaders import (
-    TextLoader,
-)
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain.chains import ConversationChain, LLMChain
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory, ConversationSummaryBufferMemory
 
+###########################################################
+# constants
 CHROMA_PERSIST_DIR = "chroma-persist"
 CHROMA_COLLECTION_NAME = "chroma_database"
 
@@ -42,19 +31,17 @@ CHROMA_COLLECTION_NAME = "chroma_database"
 # Instances
 llm = ChatOpenAI(temperature=0.1, max_tokens=2000)
 
-db = Chroma(
-    persist_directory=CHROMA_PERSIST_DIR,
-    embedding_function=OpenAIEmbeddings(),
-    collection_name=CHROMA_COLLECTION_NAME,
-)
+db = Chroma(persist_directory=CHROMA_PERSIST_DIR, embedding_function=OpenAIEmbeddings(), collection_name=CHROMA_COLLECTION_NAME,)
+
 retriever = db.as_retriever()
 
-def prepare_embedding_db(file_path):
+###########################################################
+# prepare embedding db
+def prepare_embedding_file(file_path):
     documents = TextLoader(file_path, encoding='UTF-8').load()
 
     text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     docs = text_splitter.split_documents(documents)
-    print(docs, end='\n\n\n')
 
     Chroma.from_documents(
         docs,
@@ -63,62 +50,17 @@ def prepare_embedding_db(file_path):
         persist_directory=CHROMA_PERSIST_DIR,
     )
 
+def prepare_embedding_dir(dir_path):
 
-from langchain.chains import ConversationChain, LLMChain
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import ChatPromptTemplate
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.endswith(".txt"):
+                file_path = os.path.join(root, file)
 
-intent_chain = LLMChain(llm=llm,  prompt=ChatPromptTemplate.from_template(template="""
-Your job is to determine the user message is related to the following list:
-- 카카오싱크
-- 카카오 싱크
-- KakaoSync
-- Kakao Sync
-
-Considering the user message, then return answer "YES" if it's related to the list, "NO" if it's not.
-
-
-User: {user_message}
-Answer:
-"""),  output_key="proceed", verbose=True,)
-
-search_chain = LLMChain(llm=llm, prompt=ChatPromptTemplate.from_template(template="""
-Your job is to read the following related document and answer the user's question based on the document.
-                                                                          
-<related_documents>
-{related_documents}
-</related_documents>
-
-
-User's question: {user_message}
-Answer:
-"""),  output_key="answer", verbose=True,)
-
-default_chain = ConversationChain(llm=llm, output_key="answer")
-
-def get_kakao_sync_doc():
-
-    with open("project_data_kakao_sync.txt", "r", encoding='UTF-8') as f:
-        return f.read()
-
-prepare_embedding_db("project_data_kakao_sync.txt")
-
-from pprint import pprint
-
-def ask_prompt(prompt):
-    context = dict(user_message=prompt)
-    context["input"] = context["user_message"]
-
-    proceed = intent_chain.run(context)
-
-    if proceed  == "YES":
-        context["related_documents"] = query_db(context["user_message"], use_retriever=True)
-
-        answer = search_chain(context)
-    else:
-        answer = {"answer": default_chain.run(context["user_message"])}
-
-    return answer
+                try:
+                    prepare_embedding_file(file_path)
+                except Exception as e:
+                    print("FAILED: ", file_path + f"by({e})")
 
 def query_db(query: str, use_retriever: bool = False) -> list[str]:
     if use_retriever:
@@ -130,6 +72,65 @@ def query_db(query: str, use_retriever: bool = False) -> list[str]:
 
     return str_docs
 
+###########################################################
+# LLM chains
+
+# 1. intent chain
+intent_chain = LLMChain(llm=llm,  prompt=ChatPromptTemplate.from_template(template="""
+Your job is to determine the user message is related to the following list:
+- 카카오 소셜
+- 카카오싱크
+- 카카오톡 채널
+
+Considering the user message, if the message is related to '카카오 소셜', then answer as 'kakao_social',
+if the message is related to '카카오싱크', then answer as 'kakao_sync',
+if the message is related to '카카오톡 채널', then answer as 'kakao_channel',
+if the message is related to none of them, then answer as 'others'.
+
+User: {user_message}
+Answer:
+"""),  output_key="intent", verbose=True,)
+
+# 2. default query chain
+search_chain = LLMChain(llm=llm, prompt=ChatPromptTemplate.from_template(template="""
+Prev conversation:
+{history}
+
+Your job is to read the following related document and answer the user's question based on the document.
+                                                                          
+<related_documents>
+{related_documents}
+</related_documents>
+
+User's question: {user_message}
+Answer:
+"""),  output_key="answer", verbose=True,)
+
+memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=500)
+
+default_chain = ConversationChain(llm=llm, memory=memory, output_key="answer")
+
+# files into embedding db
+prepare_embedding_dir(os.path.join(os.path.curdir, "files"))
+
+def ask_prompt(prompt):
+    context = dict(user_message=prompt)
+    context["input"] = context["user_message"]
+
+    intent = intent_chain.run(context)
+
+    if intent == "kakao_social" or intent == "kakao_sync" or intent == "kakao_channel":
+        context["related_documents"] = query_db(context["user_message"], use_retriever=True)
+        context["history"] = memory.buffer
+
+        answer = search_chain(context)
+    else:
+        answer = {"answer": default_chain.run(context["user_message"])}
+
+    return answer
+
+###########################################################
+# the app implementation
 
 class State(pc.State):
     """The app state."""
@@ -138,12 +139,11 @@ class State(pc.State):
     chat_history : str
 
     def do_ask(self):
-        self.chat_history = self.chat_history + "\n" + self.prompt
-        #self.chat_history.append(agent.run(self.prompt))
+        self.chat_history = self.chat_history + "\n\n" + self.prompt
         result = ask_prompt(self.prompt)
         print(result["answer"])
-        self.chat_history = self.chat_history + "\n" + result["answer"]
-        self.prompt = ""
+        self.chat_history = self.chat_history + "\n\n" + result["answer"]
+        self.prompt = ""    # clear the prompt input edit
 
 def index():
     return pc.center(
@@ -161,7 +161,6 @@ def index():
                     pc.input(
                         placeholder="Question",
                         on_change=State.set_prompt,
-                        value=State.prompt,
                     ),
                     pc.button("Ask", on_click=State.do_ask),
                     shadow="lg",
